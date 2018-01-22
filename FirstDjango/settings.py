881a8b61a0b4cb6992840bf511a576981fa74901
emma.vandobben <emma.vandobben@nelen-schuurmans.nl>
2018-01-22 16:58:46 +0100

    changed db name

--- FirstDjango/settings.py
+++ FirstDjango/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'FirstDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'FirstDatabase'),
+        'NAME': os.path.join(BASE_DIR, 'FirstDjango.sqlite3'),
     }
 }
 
