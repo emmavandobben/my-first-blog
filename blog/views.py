c86e3c909c22372a84ffe5eafb73b732b1807a93
emma.vandobben <emma.vandobben@nelen-schuurmans.nl>
2018-01-23 11:36:06 +0100

    Added a view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#return a function render that will render (put together) our template blog/post_list.html.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
