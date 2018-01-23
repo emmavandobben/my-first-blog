d7c2737ce29f2b0f34bccc01e6d9f6b68db4ec5c
emma.vandobben <emma.vandobben@nelen-schuurmans.nl>
2018-01-23 15:35:33 +0100

    Modified templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+#. before models import means in current directory
 
 #return a function render that will render (put together) our template blog/post_list.html.
+#the request has info we received from the user via internet.
+#'blog/post_list.html' is the template file
+#in between {} add something for the template. Here the QuerySet named posts.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
 
