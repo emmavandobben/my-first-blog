7daddd05b94d88fcf83af7ab22ee6c3a9fddd330
emma.vandobben <emma.vandobben@nelen-schuurmans.nl>
2018-02-15 16:36:16 +0100

    Customized the admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 from .models import Label
 from .models import Post
 
+
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "published_date"]
+    list_filter = ["title", "published_date"]
+    search_fields = ["title", "published_date"]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Label)
