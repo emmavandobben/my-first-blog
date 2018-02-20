0549345d5693733aadc9d283a03bc315733fe7ef
emma.vandobben <emma.vandobben@nelen-schuurmans.nl>
2018-02-20 12:02:45 +0100

    Made responses no longer hardwired to a single content type. Thus added support for format suffixes to API endpoints. Using named status codes.

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
@@ -6,3 +7,5 @@ urlpatterns = [
     url(r'^api/snippet/$', views.api_snippet_list, name='api_snippet_list'),
     url(r'^api/snippet/(?P<pk>[0-9]+)/$', views.api_snippet_detail, name='api_snippet_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
